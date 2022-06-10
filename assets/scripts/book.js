33d2385c0d7a7bfb43425eaa55f53c781c743772
AtalaySezen <atalaysezen@yahoo.com.tr>
2022-06-10 15:02:21 +0300

    rezervasyon sayfası input alanı oluşturuldu javascript tarafına geçilecek.

--- assets/scripts/book.js
+++ assets/scripts/book.js
@@ -3,3 +3,24 @@ document.getElementById('hrefLogo').addEventListener('click',()=>{
     window.location.href="./index.html"
     console.log("Yönlendiriliyor")
   })
+
+fetch("http://localhost:3000/posts", {
+     
+    method: "POST",
+     
+    body: JSON.stringify({
+        id: 15,
+        title: "deneme",
+        author: "atalay"
+    }),
+     
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
+    }
+})
+ 
+.then(response => response.json()) 
+.then(json => console.log(json));
+
+
+//Form input değerlerinin hepsinin alınması:
